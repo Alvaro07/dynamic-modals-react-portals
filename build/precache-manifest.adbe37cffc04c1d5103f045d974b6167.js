8df26cc969903c114ca9c5d32693b9b9a324aa8e
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2018-12-20 15:47:43 +0100

    Changes to deploy

---
+++ build/precache-manifest.adbe37cffc04c1d5103f045d974b6167.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "933f6c796dbda8db0190",
+    "url": "/static/js/main.933f6c79.chunk.js"
+  },
+  {
+    "revision": "a33d9a461fad2ae1a9b2",
+    "url": "/static/js/1.a33d9a46.chunk.js"
+  },
+  {
+    "revision": "933f6c796dbda8db0190",
+    "url": "/static/css/main.65e73029.chunk.css"
+  },
+  {
+    "revision": "389165462afcf52bd9da0fafed9e64f2",
+    "url": "/index.html"
+  }
+];
